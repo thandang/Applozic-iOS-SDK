cd5680ba4b6f949130ecb0402dae37361f4aaaee
Mukesh Thawani <mukesh9039@gmail.com>
2017-11-06 20:40:09 +0530

    AL-2223: Remove custom server setting from ALChatManager

--- sample-with-framework/applozicdemo/ALChatManager.m
+++ sample-with-framework/applozicdemo/ALChatManager.m
@@ -563,7 +563,6 @@
     
     [ALApplozicSettings setChatListTabTitle:@""];
     [ALApplozicSettings setProfileTabTitle:@""];
-    [ALApplozicSettings enableCustomStorageService:true];
     // Hide attachment options in chat screen
 //    NSArray * attachmentOptionToHide = @[@":audio", @":video", @":location",@":shareContact"];
 //

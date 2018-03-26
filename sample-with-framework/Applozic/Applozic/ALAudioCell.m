faa7723710d8777aee398633452c834b5a846f87
Sunil Kumar <sunil@applozic.com>
2018-03-26 21:16:28 +0530

    Add placeholder default Image until loading

--- sample-with-framework/Applozic/Applozic/ALAudioCell.m
+++ sample-with-framework/Applozic/Applozic/ALAudioCell.m
@@ -179,7 +179,7 @@
         if(alContact.contactImageUrl)
         {
             NSURL * theUrl1 = [NSURL URLWithString:alContact.contactImageUrl];
-            [self.mUserProfileImageView sd_setImageWithURL:theUrl1 placeholderImage:nil options:SDWebImageRefreshCached];
+            [self.mUserProfileImageView sd_setImageWithURL:theUrl1 placeholderImage:[ALUtilityClass getImageFromFramworkBundle:@"ic_contact_picture_holo_light.png"] options:SDWebImageRefreshCached];
         }
         else
         {

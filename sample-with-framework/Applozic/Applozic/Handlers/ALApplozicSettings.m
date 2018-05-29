4193ac2e2c1a26d8df023e2e4b5fb4b25ad74ded
adarshmishra <adarsh@applozic.com>
2018-05-29 20:13:31 +0530

    CS-67 : Conversation list filtering wtih category settings

--- sample-with-framework/Applozic/Applozic/Handlers/ALApplozicSettings.m
+++ sample-with-framework/Applozic/Applozic/Handlers/ALApplozicSettings.m
@@ -1123,4 +1123,14 @@
    return [[NSUserDefaults standardUserDefaults] valueForKey:APPLOZIC_LOCALIZABLE];
 
 }
+
++(void)setCategoryName:(NSString*)categoryName{
+    [[NSUserDefaults standardUserDefaults] setValue:categoryName forKey:AL_CATEGORY_NAME];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+}
+
++(NSString*)getCategoryName{
+    return [[NSUserDefaults standardUserDefaults] valueForKey:AL_CATEGORY_NAME];
+}
 @end

866bdceea086c9cca2167c1fede655e754d5ed2f
Mukesh Thawani <mukesh9039@gmail.com>
2018-09-10 12:25:48 +0530

    ALApplozicSettings: fix formatting error due to int type

--- sample-with-framework/Applozic/Applozic/ViewControllers/ALVOIPNotificationHandler.m
+++ sample-with-framework/Applozic/Applozic/ViewControllers/ALVOIPNotificationHandler.m
@@ -316,7 +316,7 @@ didReceiveNotificationResponse:(UNNotificationResponse *)response
 
 -(BOOL)isNotificationStale:(ALMessage*)alMessage
 {
-    ALSLog(ALLoggerSeverityInfo, @"[[NSDate date]timeIntervalSince1970] - [alMessage.createdAtTime doubleValue] ::%d", [[NSDate date]timeIntervalSince1970]*1000 - [alMessage.createdAtTime doubleValue]);
+    ALSLog(ALLoggerSeverityInfo, @"[[NSDate date]timeIntervalSince1970] - [alMessage.createdAtTime doubleValue] ::%f", [[NSDate date]timeIntervalSince1970]*1000 - [alMessage.createdAtTime doubleValue]);
     return ( ([[NSDate date] timeIntervalSince1970] - [alMessage.createdAtTime doubleValue]/1000) > 30);
 }
 
